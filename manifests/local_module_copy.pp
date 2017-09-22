b1debc8669645f08b3c12de2e6c932be758159b1
Isaac Wagner <iwagner@corelogic.com>
2017-09-22 10:26:46 -0400

    Updated so module list works as array

--- manifests/local_module_copy.pp
+++ manifests/local_module_copy.pp
@@ -1,9 +1,13 @@
 
 class clgxutil::local_module_copy (
   String $install_dir = $clgxutil::local_module_copy::params::install_dir,
-  $module_list = {}
+  $module_list = []
 ) inherits clgxutil::local_module_copy::params {
 
-  create_resources('clgxutil::local_module_copy::module', $module_list)
+
+  # function call with lambda:
+  $module_list.each |String $module_name| {
+    clgxutil::local_module_copy::module { $module_name: }
+  }
 
 }
