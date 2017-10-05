3d43c7f0ad9280be47b2802c3a5f84222cbcbbfa
Isaac Wagner <iwagner@corelogic.com>
2017-10-04 21:23:59 -0400

    fixed resource namimg to remove confict

--- manifests/imageprep/install_module.pp
+++ manifests/imageprep/install_module.pp
@@ -6,7 +6,7 @@ define clgxutil::imageprep::install_module {
     $abs_dest = "${::clgxutil::imageprep::global_module_dir}/${title}"
 
 
-    file { "modulecopy-${title}":
+    file { "modulecleanup-${title}":
       ensure  => directory,
       path    => $abs_dest,
       source  => $abs_source,
