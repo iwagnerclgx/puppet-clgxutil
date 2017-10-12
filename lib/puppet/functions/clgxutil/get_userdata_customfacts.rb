e771fc53a251ddef212cb209bd85e3b5d0d619af
Isaac Wagner <iwagner@corelogic.com>
2017-10-12 12:45:56 -0400

    fixed very broken function

--- lib/puppet/functions/clgxutil/get_userdata_customfacts.rb
+++ lib/puppet/functions/clgxutil/get_userdata_customfacts.rb
@@ -9,23 +9,34 @@ Puppet::Functions.create_function(:'clgxutil::get_userdata_customfacts') do
   end
 
   require 'net/http'
+  require "base64"
+  require "json"
 
   def get()
 
+    userdata_text = ""
     begin
-      $text = Net::HTTP.get('169.254.169.254', '/latest/user-data')
+      userdata_text = Net::HTTP.get('169.254.169.254', '/latest/user-data')
     rescue
       # Any errors probably mean we are not on EC2
       nil
     end
 
-    $matches = /custom_facts=([a-zA-Z0-9]+=)/.match($text)
-    if $matches then
+
+    matches = /custom_facts=([a-zA-Z0-9=]+)/.match(userdata_text)
+    if matches then
       begin
-        $decoded = Base64.decode64($matches[1])
-        JSON.parse($decoded)
+        combined = Hash[]
+        decoded = Base64.decode64(matches[1])
+        json_decoded = JSON.parse(decoded)
+
+        # construct return Hash that can be used with create_resources
+        json_decoded.each do |name, value|
+          combined[name] = Hash["name" => name, "value" => value]
+        end
+        combined
       rescue
-        warning("Error in parsing userdata json")
+        call_function('warning', 'clgxutil::get_userdata_customfacts could not decode userdata json')
         nil
       end
     end
